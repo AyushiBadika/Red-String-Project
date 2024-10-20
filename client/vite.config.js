7361c05aca573d145e6f367fbe6cd43c31c1c44d
AyushiBadika <badikaayushi@gmail.com>
2024-10-20 12:42:37 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {},
   plugins: [react()],
   optimizeDeps: {
-    include: ["react", "react-dom"],
+    include: ["react", "react-dom", "redux-persist/integration/react"],
   },
 });
